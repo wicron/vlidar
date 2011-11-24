1b4507c4e28c2093d88b66aff85f16ae71e61133
pavel <pavel@linux-2799.site>
2011-11-24 23:30:35 +0400

    Refactoring build system.
    Add abilities to find urg drivers and Qwt library.
    Add dependence on Qwt libary.

--- cmake/FindURG.cmake
+++ cmake/FindURG.cmake
@@ -1,18 +1,27 @@
-#FIND_PACKAGE(c_urg REQUIRED)
-FIND_PATH(URG_INCLUDE_DIR NAMES urg/UrgDevice.h)
-MESSAGE(STATUS "FOUND URG headers =" ${URG_INCLUDE_DIR})
-INCLUDE_DIRECTORIES(${URG_INCLUDE_DIR})
+# - Try to find the Urg includes and library
+# which defines
+#
+# URG_FOUND - system has Urg
+# URG_INCLUDE_DIR - where to find UrgDevice.h
+# URG_LIBRARIES - the libraries to link against to use Urg
 
-#Fix Me: Find library doesn't work for me with few names
-#FIND_LIBRARY(URG_LIBRARIES NAMES c_urg c_urg_system c_urg_connection PATH /usr/local/lib)
+SET(URG_FOUND FALSE)
+
+FIND_PATH(URG_INCLUDE_DIR NAMES urg/UrgDevice.h)
+
 FIND_LIBRARY(URG_LIBRARY NAMES c_urg PATH /usr/local/lib)
 FIND_LIBRARY(URG_SYSTEM_LIBRARY NAMES c_urg_system PATH /usr/local/lib)
 FIND_LIBRARY(URG_CONNECTION_LIBRARY NAMES c_urg_connection PATH /usr/local/lib)
 SET(URG_LIBRARIES ${URG_SYSTEM_LIBRARY} ${URG_LIBRARY} ${URG_CONNECTION_LIBRARY})
-MESSAGE(STATUS "Found URG_LIBRARIES ="${URG_LIBRARIES})
 
+IF(URG_LIBRARIES)
 SET(URG_FOUND TRUE)
+ENDIF(URG_LIBRARIES)
 
-IF(NOT URG_LIBRARIES)
-SET(URG_FOUND FALSE)
-ENDIF(NOT URG_LIBRARIES)
+IF(URG_FOUND)
+    MESSAGE(STATUS "Found c_urg libraries. " ${URG_LIBRARIES})
+ELSE(URG_FOUND)
+    IF(URG_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could'not find URG library")
+    ENDIF(URG_FIND_REQUIRED)
+ENDIF(URG_FOUND)
